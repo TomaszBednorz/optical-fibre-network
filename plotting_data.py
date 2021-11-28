4297885676f08fe28590b021eaadf71fef17b867
Michal_Spinczyk <michal.spinczyk@gmail.com>
2021-11-28 01:33:28 +0100

    Created visualization in plotting_data.py, added data to network.py, call visualization in main.py

--- plotting_data.py
+++ plotting_data.py
@@ -1,24 +1,33 @@
 import gmplot
+from data_structures import *
 
-# Create the map plotter:
-apikey = 'AIzaSyBal6A70lGi745Rm8Fdk0o5FZEleeHhBLI' # (your API key here)
-gmap = gmplot.GoogleMapPlotter(50.165997404672005, 19.625832486967628, 17, apikey=apikey)
+def visualization(buildings: list,poles: list,edges: list = None):
+    # Create the map plotter:
+    apikey = 'AIzaSyBal6A70lGi745Rm8Fdk0o5FZEleeHhBLI' # (your API key here)
+    gmap = gmplot.GoogleMapPlotter(50.165997404672005, 19.625832486967628, 17, apikey=apikey)
 
-# Highlight buildings:
-building_y, building_x = zip(*[
-    (50.16542348975432, 19.624805930923117),
-    (50.16521551792717, 19.624896933788143),
-    (50.16538803559229, 19.624432547030786),
-    (50.16512627770332, 19.623939738063434)])
+    # Highlight buildings:
+    for i in range(len(buildings)):
+        buildings[i] = (buildings[i].vert_coord,buildings[i].hori_coord)
 
-gmap.scatter(building_y, building_x, color='#3B0B39', size=3, marker=False)
+    building_y, building_x = zip(*buildings)
 
-# Create connection between two buildings
-path = zip(*[
-    (50.16542348975432, 19.624805930923117),
-    (50.16521551792717, 19.624896933788143)])
+    gmap.scatter(building_y, building_x, color='r', size=4, marker=False)
 
-gmap.plot(*path, edge_width=1, color='red')
+    # Highlight poles:
+    for i in range(len(poles)):
+        poles[i] = (poles[i].vert_coord,poles[i].hori_coord)
 
-# Draw the map to an HTML file:
-gmap.draw('map.html')
+    poles_y, poles_x = zip(*poles)
+
+    gmap.scatter(poles_y, poles_x, color='k', size=2, marker=False, symbol = 'x')
+
+    # Create connection between two buildings
+    path = zip(*[
+        (50.16542348975432, 19.624805930923117),
+        (50.16521551792717, 19.624896933788143)])
+
+    gmap.plot(*path, edge_width=4, color='b')
+
+    # Draw the map to an HTML file:
+    gmap.draw('map.html')
